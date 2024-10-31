7623affa65f1bf3f1d0a899747331654ff77f2ad
Lrigami <prune.thiry@hotmail.com>
2024-10-31 10:43:23 +0100

    split app.js into multiple js files

---
+++ Certif-ResponsiveWebDesign/FourthProject-ProductLandingPage/scripts/app.js
@@ -0,0 +1,44 @@
+const translate = document.querySelectorAll(".translate");
+const translateInverted = document.querySelectorAll(".translateInverted");
+const shadow = document.querySelectorAll(".shadow");
+const navbar = document.getElementById("nav-bar");
+const joinUsButton = document.getElementById("join-us-button");
+
+joinUsButton.onclick = () => {
+    window.location.href = "#JoinUs";
+}
+
+window.addEventListener("scroll", () => {
+    let scroll = window.scrollY;
+    
+    // parallax effect
+    translate.forEach(element => {
+        let speed = element.dataset.speed;
+        element.style.transform = `translateY(${scroll * speed}px)`;
+    })
+
+    translateInverted.forEach(element => {
+        let speed = element.dataset.speed;
+        element.style.transform = `translateY(-${scroll * speed}px)`;
+    });
+
+    // Change opacity of scroll-down-circles depending on scrollY
+    shadow.forEach(element => {
+        let opacity = element.dataset.opacity;
+        element.style.opacity = `${opacity - ((scroll/100)/8)}`;
+    })
+
+    // Change navbar background opacity depending on scrollY
+    let navbarNoopacity = navbar.dataset.noopacity;
+    let newNavbarOpacity = `${navbarNoopacity + ((scroll/100)/8)}`;
+    let currentNavbarColor = getComputedStyle(navbar).backgroundColor;
+    let rgbaNavbarColor = currentNavbarColor.replace(/rgba?\((\d+), (\d+), (\d+),? ?[\d\.]*\)/, `rgba($1, $2, $3, ${newNavbarOpacity})`);
+    navbar.style.backgroundColor = rgbaNavbarColor;
+
+    // Change navbar Join Us button border opacity depending on scrollY
+    let joinUsButtonOpacity = joinUsButton.dataset.noopacity;
+    let newJoinUsButtonOpacity = `${joinUsButtonOpacity + + ((scroll/100)/8)}`;
+    let currentJoinUsButtonBorderColor = getComputedStyle(joinUsButton).borderColor;
+    let rgbaJoinUsButtonBorderColor = currentJoinUsButtonBorderColor.replace(/rgba?\((\d+), (\d+), (\d+),? ?[\d\.]*\)/, `rgba($1, $2, $3, ${newJoinUsButtonOpacity})`);
+    joinUsButton.style.borderColor = rgbaJoinUsButtonBorderColor;
+})
