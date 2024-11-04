354f7bb7cb8aa968c0a035e7cf34c32dbe1d7a14
Lrigami <prune.thiry@hotmail.com>
2024-11-04 16:22:17 +0100

    about me section

--- Certif-ResponsiveWebDesign/FifthProject-Portfolio/app.js
+++ Certif-ResponsiveWebDesign/FifthProject-Portfolio/app.js
@@ -1,10 +1,27 @@
 let parallax = document.querySelectorAll(".parallax");
-
+let parallaxL = document.querySelectorAll(".parallaxL");
+let parallaxR = document.querySelectorAll(".parallaxR");
+let parallaxB = document.querySelectorAll(".parallaxB");
 
 window.addEventListener("scroll", () => {
     let scroll = window.scrollY;
     parallax.forEach((element) => {
         let speed = element.dataset.speed;
         element.style.transform = `translateX(${scroll * speed}px)`;
+    })
+
+    parallaxL.forEach((element) => {
+        let speed = element.dataset.speed;
+        element.style.transform = `translateX(-${scroll * speed}px)`;
+    })
+
+    parallaxR.forEach((element) => {
+        let speed = element.dataset.speed;
+        element.style.transform = `translateX(${scroll * speed}px)`;
+    })
+
+    parallaxB.forEach((element) => {
+        let speed = element.dataset.speed;
+        element.style.transform = `translateY(${scroll * speed}px)`;
     })
 })
