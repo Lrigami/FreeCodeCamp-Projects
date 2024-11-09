940bc1c47793bf9ee5632a600984a5c4a7547a6a
Lrigami <prune.thiry@hotmail.com>
2024-11-09 11:31:09 +0100

    supp useless code for parallax effect

--- Certif-ResponsiveWebDesign/FifthProject-Portfolio/app.js
+++ Certif-ResponsiveWebDesign/FifthProject-Portfolio/app.js
@@ -1,15 +1,11 @@
+// parallax effects
 let parallax = document.querySelectorAll(".parallax");
 let parallaxL = document.querySelectorAll(".parallaxL");
 let parallaxR = document.querySelectorAll(".parallaxR");
 let parallaxB = document.querySelectorAll(".parallaxB");
 
-// parallax effects
 window.addEventListener("scroll", () => {
     let scroll = window.scrollY;
-    parallax.forEach((element) => {
-        let speed = element.dataset.speed;
-        element.style.transform = `translateX(${scroll * speed}px)`;
-    })
 
     parallaxL.forEach((element) => {
         let speed = element.dataset.speed;
