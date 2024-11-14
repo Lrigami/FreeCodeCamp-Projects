d50e6b2e1ceeb3b2fd4ee94d19c1ee1d0d6ebe30
Lrigami <prune.thiry@hotmail.com>
2024-11-14 14:33:59 +0100

    simplified code

--- Certif-JavaScriptAlgorithmsAndDataStructures/FirstProject-PalindromeChecker/script.js
+++ Certif-JavaScriptAlgorithmsAndDataStructures/FirstProject-PalindromeChecker/script.js
@@ -2,21 +2,31 @@ let textInput = document.getElementById("text-input");
 let checkBtn = document.getElementById("check-btn");
 let resultP = document.getElementById("result");
 
+// function checkPalindrome() {
+//     if (textInput.value == "") {
+//         window.alert("Please input a value");
+//     } else {
+//         let checkedText = textInput.value.toString().toLowerCase().replace(/[^A-Z0-9]/ig, "");
+//         let a = [], b = [];
+//         for (let i = 0 ; i < checkedText.length ; i++) {
+//             a.push(checkedText[i]);
+//             b.push(checkedText[checkedText.length-(i+1)]);
+//         }
+//         resultP.style.display = "block"; 
+//         if (a.toString() === b.toString()) {
+//             resultP.innerText = `"${textInput.value}" is a palindrome`;
+//         } else {
+//             resultP.innerText = `"${textInput.value}" is not a palindrome`;
+//         }
+//     }
+// };
+
 function checkPalindrome() {
     if (textInput.value == "") {
         window.alert("Please input a value");
     } else {
-        let checkedText = textInput.value.toString().toLowerCase().replace(/[^A-Z0-9]/ig, "");
-        let a = [], b = [];
-        for (let i = 0 ; i < checkedText.length ; i++) {
-            a.push(checkedText[i]);
-            b.push(checkedText[checkedText.length-(i+1)]);
-        }
+        let checkedText = textInput.value.toString().toLowerCase().replace(/[^a-zA-Z]/ig, "");
         resultP.style.display = "block"; 
-        if (a.toString() === b.toString()) {
-            resultP.innerText = `"${textInput.value}" is a palindrome`;
-        } else {
-            resultP.innerText = `"${textInput.value}" is not a palindrome`;
-        }
+        resultP.innerText = checkedText.split("").toString() === checkedText.split("").reverse().toString() ? `"${textInput.value}" is a palindrome` : `"${textInput.value}" is not a palindrome`;
     }
 };
