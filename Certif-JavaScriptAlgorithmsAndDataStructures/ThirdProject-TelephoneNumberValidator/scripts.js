9512dbd452ec9c5c9d95089e4505a2d2db4b5149
Lrigami <prune.thiry@hotmail.com>
2024-12-19 10:51:38 +0100

    project done

--- Certif-JavaScriptAlgorithmsAndDataStructures/ThirdProject-TelephoneNumberValidator/scripts.js
+++ Certif-JavaScriptAlgorithmsAndDataStructures/ThirdProject-TelephoneNumberValidator/scripts.js
@@ -3,22 +3,17 @@ const check = document.getElementById("check-btn");
 const clear = document.getElementById("clear-btn");
 const result = document.getElementById("results-div");
 
-const isInputValid = input => {
-    let val = input.value;
-    return val === "" ? false : true;
-}
-
 check.addEventListener("click", () => {
-    if (isInputValid) {
-        let regex = /^([1][\s|-]?)?(\d{3}[\s|-]?|\(\d{3}\)[\s|-]?)(\d{3}[\s|-]?)(\d{4})/;
+    if (input.value !== "") {
+        let regex = /^([1][\s|-]?)?(\d{3}[\s|-]?|\(\d{3}\)[\s|-]?)(\d{3}[\s|-]?)(\d{4})$/;
         if (regex.test(input.value)) {
           result.innerText = `Valid US number: ${input.value}`;
         } else {
           result.innerText = `Invalid US number: ${input.value}`;
         }
     } else {
-        result.innerText = "Please provide a phone number";
+        alert("Please provide a phone number");
     }
 })
 
-clear.addEventListener("click", results.innerText = "");
+clear.addEventListener("click", () => result.innerText = "");
